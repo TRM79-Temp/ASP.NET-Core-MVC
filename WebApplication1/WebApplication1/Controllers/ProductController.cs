2dc00d157791a10b799a252bf2680221dab296d0
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2019-02-26 15:58:04 +0200

    Added pagination.

--- WebApplication1/WebApplication1/Controllers/ProductController.cs
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -1,17 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using System.Linq;
 
 namespace WebApplication1.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize { get; set; } = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1)
+            => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
     }
 }
