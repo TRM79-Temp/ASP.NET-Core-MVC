2b5d116bd7136d7f4dc448273babe953011720ac
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2019-03-01 16:45:57 +0200

    ...

---
+++ WebApplication1/WebApplication1/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
