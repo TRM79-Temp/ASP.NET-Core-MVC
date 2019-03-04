e2b9c40aeec8b05939cc272f24a05cb79994d651
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2019-03-04 15:23:51 +0200

    p. 256 complete

--- WebApplication1/WebApplication1.Tests/NavigationMenuViewComponentTests.cs
+++ WebApplication1/WebApplication1.Tests/NavigationMenuViewComponentTests.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
 using Moq;
 using WebApplication1.Components;
 using WebApplication1.Models;
@@ -32,6 +34,32 @@ namespace WebApplication1.Tests
                 "Oranges",
                 "Plums"
             }, results));
+        }
+
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            // Arrange
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product { ProductID = 4, Name = "P2", Category = "Oranges" },
+            }).AsQueryable<Product>());
+            NavigationMenuViewComponent target =
+                new NavigationMenuViewComponent(mock.Object);
+            target.ViewComponentContext = new ViewComponentContext {
+                ViewContext = new ViewContext {
+                    RouteData = new RouteData()
+                }
+            };
+            target.RouteData.Values["category"] = categoryToSelect;
+            // Action
+            string result = (string) (target.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+            // Assert
+            Assert.Equal(categoryToSelect, result);
         }
     }
 }
